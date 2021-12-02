8a78f975aca35d773d918c4cbe3289db51234574
Maxim <50755616+Giveruup@users.noreply.github.com>
2021-12-02 10:27:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    // scroll
+    $('[data-scroll]').on('click', function (event) {
+        event.preventDefault();
+
+        let elementID = $(this).data('scroll');
+        let elementOffset = $(elementID).offset().top;
+
+        $('html, body').animate({
+            scrollTop: elementOffset - 50
+        }, 700);
+    });
+
+
+});
