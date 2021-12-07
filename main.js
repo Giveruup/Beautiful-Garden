d5aa7ecfe0a37b84f8744416bbbb2fa7e4cfde56
Maxim <50755616+Giveruup@users.noreply.github.com>
2021-12-07 14:50:10 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 $(function () {
 
+    let nav = $('#nav');
+    let navToggle = $('#navToggle');
+
     // scroll
     $('[data-scroll]').on('click', function (event) {
         event.preventDefault();
@@ -7,10 +10,21 @@ $(function () {
         let elementID = $(this).data('scroll');
         let elementOffset = $(elementID).offset().top;
 
+        nav.removeClass('show');
+        navToggle.removeClass('active');
+
         $('html, body').animate({
             scrollTop: elementOffset - 50
         }, 700);
     });
 
+    // nav toggle
 
+
+    navToggle.on('click', function (event) {
+        event.preventDefault();
+
+        nav.toggleClass('show');
+        navToggle.toggleClass('active');
+    });
 });
